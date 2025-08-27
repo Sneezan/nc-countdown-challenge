c8cc37d9019beb685349d8a6cdcab68707bb69a7
Sneezan <ceciliafraanlund@gmail.com>
2025-08-27 11:59:40 +0200

    added basic logic for countdown

--- countdown-app/src/app/app.config.ts
+++ countdown-app/src/app/app.config.ts
@@ -3,16 +3,13 @@ import {
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
     provideClientHydration(withEventReplay()),
   ],
 };
--- countdown-app/src/app/app.ts
+++ countdown-app/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CountdownComponent } from './components/CountDown/countdown.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CountdownComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
